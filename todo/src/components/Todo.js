7730d9b556ae39d8b72ee74e89265ec2c856453c
Hanen Alrwasheda <hanenalrwasheda.off@gmail.com>
2021-07-30 19:00:44 +0200

    created add() to add items after entering them using input field. add-btn adds them and creates del- and arch-btn and clears the input field

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -2,9 +2,9 @@ import "../css/TodoStyle.css"
 export default function Todo(){
     
     return(
-        <div className="main-container">
-            <h1 className="title">TODO</h1>
-            <div className="content-container"></div>
+        <div id="main-container">
+            <h1 id="title">TODO</h1>
+            <div id="content-container"></div>
         </div>
     );
 }
